0a95195ce634b46224ee10b805d58dae27821804
ericyork <eyork@clarkson.edu>
2020-02-29 19:59:02 -0500

    last minutes corrections and final comments

--- js/forge.js
+++ js/forge.js
@@ -1,8 +1,11 @@
+// Creates a variable to save me from writing document.getElementById() over and over.
+const $ = document.getElementById();
+
 // Simple Constructor Pattern
-// Creates a basic interface for creating objects
+// Sets up a basic interface for creating objects
 class Hero {
   constructor(name, secretIdentity) {
-    // setting property values
+    // setting property values; underscore used as a convention for unique identifier.
     this._name = name;
     this._secretIdentity = secretIdentity;
 
@@ -18,13 +21,13 @@ const SpiderMan = new Hero('Spiderman', 'Peter Parker');
 const WonderWoman = new Hero('Wonder Woman', 'Diana Prince');
 
 function revealIronMan() {
-  document.getElementById("ironman").innerHTML = (IronMan.revealIdentity());
+  $("ironman").innerHTML = (IronMan.revealIdentity());
 }
 function revealSpiderMan() {
-  document.getElementById("spiderman").innerHTML = (SpiderMan.revealIdentity());
+  $("spiderman").innerHTML = (SpiderMan.revealIdentity());
 }
 function revealWonderWoman() {
-  document.getElementById("wonder-woman").innerHTML = (WonderWoman.revealIdentity());
+  $("wonder-woman").innerHTML = (WonderWoman.revealIdentity());
 }
 
 // Factory Pattern
@@ -64,22 +67,22 @@ const ironMansPowers = factory.makePower('fist');
 const spiderMansPowers = factory.makePower('punch');
 const wonderWomansPowers = factory.makePower('kick');
 function ironManAction() {
-  document.getElementById("ironman-action").innerHTML = (ironMansPowers.action());
+  $("ironman-action").innerHTML = (ironMansPowers.action());
 }
 function ironManStun() {
-  document.getElementById("ironman-stun").innerHTML = (ironMansPowers.stun());
+  $("ironman-stun").innerHTML = (ironMansPowers.stun());
 }
 function spiderManAction() {
-  document.getElementById("spiderman-action").innerHTML = (spiderMansPowers.action());
+  $("spiderman-action").innerHTML = (spiderMansPowers.action());
 }
 function spiderManStun() {
-  document.getElementById("spiderman-stun").innerHTML = (spiderMansPowers.stun());
+  $("spiderman-stun").innerHTML = (spiderMansPowers.stun());
 }
 function wonderWomanAction() {
-  document.getElementById("wonder-woman-action").innerHTML = (wonderWomansPowers.action());
+  $("wonder-woman-action").innerHTML = (wonderWomansPowers.action());
 }
 function wonderWomanTrip() {
-  document.getElementById("wonder-woman-trip").innerHTML = (wonderWomansPowers.trip());
+  $("wonder-woman-trip").innerHTML = (wonderWomansPowers.trip());
 }
 
 // Singleton Pattern
@@ -95,7 +98,7 @@ class Victories {
 // usage
 let pageCounter = new Victories();
 function addVictory() {
-  document.getElementById("display").innerHTML = (pageCounter.add());
+  $("display").innerHTML = (pageCounter.add());
 }
 //
 // Decorator Pattern
@@ -137,23 +140,23 @@ function aiEnhance(stats) {
 // usage
 let ironManStats = new Stats(8, 12, 5);
 function displayStats() {
-  document.getElementById("offense").innerHTML = (ironManStats.offense);
-  document.getElementById("defense").innerHTML = (ironManStats.defense);
-  document.getElementById("base-speed").innerHTML = (ironManStats.speed);
+  $("offense").innerHTML = (ironManStats.offense);
+  $("defense").innerHTML = (ironManStats.defense);
+  $("base-speed").innerHTML = (ironManStats.speed);
 }
 function donPowerArmor() {
   let buffedStats = armorOn(ironManStats);
-  document.getElementById("defense").innerHTML = (buffedStats.defense);
-  document.getElementById("base-speed").innerHTML = (buffedStats.speed);
-  document.getElementById("fly-move").innerHTML = (buffedStats.active);
-  document.getElementById("fly-speed").innerHTML = "Fly Speed";
-  document.getElementById("fly-speed-value").innerHTML = (buffedStats.flySpeed);
+  $("defense").innerHTML = (buffedStats.defense);
+  $("base-speed").innerHTML = (buffedStats.speed);
+  $("fly-move").innerHTML = (buffedStats.active);
+  $("fly-speed").innerHTML = "Fly Speed";
+  $("fly-speed-value").innerHTML = (buffedStats.flySpeed);
 }
 function enableJarvis() {
   let enhancedStats = aiEnhance(ironManStats);
-  document.getElementById("offense").innerHTML = (enhancedStats.offense);
-  document.getElementById("defense").innerHTML = (enhancedStats.defense);
-  document.getElementById("base-speed").innerHTML = (enhancedStats.speed);
-  document.getElementById("fly-speed-value").innerHTML = (enhancedStats.flySpeed);
-  document.getElementById("foresight").innerHTML = (enhancedStats.enhanced);
+  $("offense").innerHTML = (enhancedStats.offense);
+  $("defense").innerHTML = (enhancedStats.defense);
+  $("base-speed").innerHTML = (enhancedStats.speed);
+  $("fly-speed-value").innerHTML = (enhancedStats.flySpeed);
+  $("foresight").innerHTML = (enhancedStats.enhanced);
 }
